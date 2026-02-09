602007935a5b4d89ff6b195a6a043ad4bb7ae302
rafaelsoares38pro <rafaelsoares38pro@gmail.com>
2026-02-09 22:55:43 +0000

    responsividade 3

--- coffe/script.js
+++ coffe/script.js
@@ -4,9 +4,16 @@ document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
 
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.onclick = () => {
+        navbar.classList.remove('active');
+    }
+});
+
 let backToTopBtn = document.querySelector('.back-to-top-btn');
 
 window.onscroll = () => {
+    navbar.classList.remove('active');
     if (window.scrollY > 100) { 
         backToTopBtn.classList.add('active');
     } else {
